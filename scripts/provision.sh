f4567983460e3fa86c357a52eac870e4a0ca9feb
Nikita Koval <me@nikitakoval.com>
2017-12-06 11:48:44 +0600

    Понизил версию mysql, поправил конфиг nginx

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -164,7 +164,7 @@ server {
 
     try_files \$uri/index.html \$uri @puma;
     location / {
-        proxy_pass http://@puma;
+        proxy_pass http://puma;
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
         proxy_set_header Host \$http_host;
         proxy_redirect off;
